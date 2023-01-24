b397ee21d82de8d88e8d3fff4377c990a8d8c26b
eanternet <gathrean@icloud.com>
2023-01-24 14:43:32 -0800

    Organized the files.

--- Assignments/Assignment3/Comp1510-assign-3-gathrean/dist/run.bat
+++
@@ -1,7 +0,0 @@
-java -jar q1.jar
-pause
-java -jar q2.jar
-pause
-java -jar q3.jar
-pause
-
--- Assignments/Assignment3/Comp1510-assign-3-gathrean/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module ca.bcit.comp1510f.athree {
-    requires org.junit.jupiter.api;
-
-    exports q1;
-    exports q2;
-    exports q3;
-}
